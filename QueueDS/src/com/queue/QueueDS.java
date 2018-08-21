74e7744a84bd30a2abeea0f0902c951864e9d215
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2018-08-21 20:48:51 +0530

    isempty and isfull method implemented

--- QueueDS/src/com/queue/QueueDS.java
+++ QueueDS/src/com/queue/QueueDS.java
@@ -10,16 +10,39 @@ public class QueueDS {
 	int front;
 
 	public void enqueue(int data) {
-
-		queue[rear] = data;
-		rear = rear + 1;
-		size = size + 1;
+		if (!isfull()) {
+			queue[rear] = data;
+			rear = rear + 1;
+			size = size + 1;
+		}
+		else {
+			System.out.println("queue is full");
+		}
 	}
 
-	public int dequeue() {
-		int data = queue[front];
-		front = front + 1;
-		size = size - 1;
-		return data;
+	public void dequeue() {
+		if (!isempty()) {
+			int data = queue[front];
+			front = front + 1;
+			size = size - 1;
+		}
+		// return data;
+		else {
+			System.out.println("queue is empty");
+		}
+	}
+
+	public void show() {
+		for (int i = front; i < queue.length; i++) {
+			System.out.println(queue[i]);
+		}
+	}
+
+	public boolean isempty() {
+		return (size == 0);
+	}
+
+	public boolean isfull() {
+		return (size == 5);
 	}
 }
